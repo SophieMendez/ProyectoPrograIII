273f0ec26604dc7509e0b68d303fc0ed574b90aa
SophieMendez <144861582+SophieMendez@users.noreply.github.com>
2023-12-11 14:33:47 -0600

    Add files via upload

---
+++ api/src/main/java/com/api/api/DatabaseService.java
@@ -0,0 +1,114 @@
+package com.api.api;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseService {// inicio de llaves class
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+
+    //@Autowired
+    public DatabaseService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+     public List<Notas> getAllNotes() {
+        try {
+            String query = "SELECT * FROM proyectoapi.nota";
+            List<Map<String, Object>> respNotes = jdbcTemplate.queryForList(query);
+            List<Notas> notes = new ArrayList<>();
+ 
+            for (Map<String, Object> row : respNotes) {
+                int id_nota = (int) row.get("id_nota");
+                String nombre_nota = (String) row.get("nombre_nota");
+                String estado_nota = (String) row.get("estado_nota");
+                String descripcion_nota = (String) row.get("descripcion_nota");
+ 
+                Notas notas = new Notas(id_nota, nombre_nota, estado_nota, descripcion_nota);
+                notes.add(notas);
+            }
+            return notes;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //Inicio login
+    public User authenticateUser(String username, String password) {
+        System.out.println("logId = " + username);
+        try {
+            String query = "SELECT * proyectoapi.users WHERE Username = ? and Password =?";
+
+            return jdbcTemplate.queryForObject(query, (rs, rowNum) -> {
+                int userID = (int)rs.getInt("UserID");
+                String user_name = rs.getString("Username");
+                String user_password = rs.getString("Password");
+                String email = rs.getString("Email");
+              //   public User( String username,String email, String password) {
+        
+                return new User(userID, user_name,email, user_password);
+            }, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    //Final login
+
+    public Notas getNotas(int id){
+        System.out.println("logId = " + id);
+        try {
+            String query = "SELECT * FROM notas WHERE id_nota = ?";
+ 
+            return jdbcTemplate.queryForObject(query, (rs, rowNum) -> {
+                int id_nota = (int)rs.getInt("id_nota");
+                String nombre_nota = rs.getString("nombre_nota");
+                String estado_nota = rs.getString("estado_nota");
+                String descripcion_nota = rs.getString("descripcion_nota");
+              
+                return new Notas(id_nota, nombre_nota, estado_nota, descripcion_nota);
+            }, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void updateNotas(Notas notas) {
+        try {
+            String query = "UPDATE notas SET nombre_nota = ?, estado_nota = ?, descripcion_nota = ? WHERE id_nota = ?";
+            jdbcTemplate.update(query, notas.getNombre_nota(),notas.getEstado_nota(),notas.getDescripcion_nota(), notas.getId_nota());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertNotas(Notas notas) {
+        try {
+            String query = "INSERT notas SET nombre_nota = ?, estado_nota = ?, descripcion_nota = ? ";
+            jdbcTemplate.update(query, notas.getNombre_nota(),notas.getEstado_nota(),notas.getDescripcion_nota(), notas.getId_nota());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int deleteNotas(int id) {
+        try {
+            String query = "DELETE FROM notas WHERE id_nota = ?";
+            jdbcTemplate.update(query, id);
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+    
+}//Final de la llave class
--- api/src/main/java/com/api/api/User.java
+++ api/src/main/java/com/api/api/User.java
@@ -1,36 +1,82 @@
 package com.api.api;
 
-import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+public class User {
 
-public class User implements Serializable {
+    private int UserID;
+    private String  Username;
+    private String Email;
+    private String Password;
+    private String JWT;
+    private String mySecreString= "unPassword Super Fuerte";
 
-    private final int id;
-    private final String username;
-    private final String password;
+    public User() {
+        this.UserID = 0;
+        this.Username = "";
+        this.Email = "";
+        this.Password = "";
+    }
 
-    public User(int id, String username, String password) {
-        // Validación de datos (puedes personalizar esto según tus necesidades)
-        if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
-            throw new IllegalArgumentException("Username and password cannot be null or empty");
+    public User(int userID,String username,String email, String password) {
+        this.UserID = userID;
+        this.Username = username;
+        this.Email = email;
+        this.Password = password;
+        this.JWT = generateJsonWebToken(username, password);
+    }
+   public User( String username,String email, String password) {
+        
+        this.Username = username;
+        this.Email = email;
+        this.Password = password;
+        this.JWT = generateJsonWebToken(username, password);
+  
+    }
+    public int getUserID() {
+        return this.UserID;
+    }
+
+    public String getEmail() {
+        return this.Email;
+    }
+
+     public boolean checkPassword(String password) {
+        return this.Password.equals(password);
+    }
+
+    public void setPassword(String password) {
+        this.Password = password;
+    }   
+
+    public void setJTW() {
+        this.JWT = generateJsonWebToken(this.Username, this.Password);
+    }  
+   
+    private String generateJsonWebToken(String username, String password) {
+        try {
+            
+            String dataToHash = ""+username + password + mySecreString;
+            System.out.println("jtw = " + username);
+     
+            
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = digest.digest(dataToHash.getBytes(StandardCharsets.UTF_8));
+
+            
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashedBytes) {
+                hexString.append(String.format("%02x", b));
+            }
+            System.out.println("jtw = " + hexString.toString());
+     
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null; 
         }
-
-        this.id = id;
-        this.username = username;
-        this.password = password;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
- 
+    
 }
